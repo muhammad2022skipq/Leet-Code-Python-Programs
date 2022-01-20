684b620aba213d04b5d325c138a3c31e7345c4fe
muhammad2022skipq <97848367+muhammad2022skipq@users.noreply.github.com>
2022-01-21 03:11:01 +0500

    Update 6PalindromeNumber.py

--- 6PalindromeNumber.py
+++ 6PalindromeNumber.py
@@ -1,6 +1,6 @@
-Given an integer x, return true if x is palindrome integer.
+#Given an integer x, return true if x is palindrome integer.
 
-An integer is a palindrome when it reads the same backward as forward.
+#An integer is a palindrome when it reads the same backward as forward.
 
 For example, 121 is a palindrome while 123 is not.
 class Solution(object):
