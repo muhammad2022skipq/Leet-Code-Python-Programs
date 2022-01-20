bb7d40667a5b57d552975845a68dba399fdfc900
muhammad2022skipq <97848367+muhammad2022skipq@users.noreply.github.com>
2022-01-21 03:10:00 +0500

    Update 4UncommonWordsfromTwoSentences.py

--- 4UncommonWordsfromTwoSentences.py
+++ 4UncommonWordsfromTwoSentences.py
@@ -1,8 +1,8 @@
-A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+#A sentence is a string of single-space separated words where each word consists only of lowercase letters.
 
-A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+#A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
 
-Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+#Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
 
 class Solution(object):
     def uncommonFromSentences(self, s1, s2):
