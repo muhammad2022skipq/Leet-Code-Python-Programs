2ecf5a1a94b398e22ba45a06e33a04752e48fbc9
muhammad2022skipq <97848367+muhammad2022skipq@users.noreply.github.com>
2022-01-21 03:09:37 +0500

    Update 3TwoSum.py

--- 3TwoSum.py
+++ 3TwoSum.py
@@ -1,10 +1,10 @@
-Description:  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#Description:  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+#You can return the answer in any order.
 
-Python Code:
+#Python Code:
 class Solution(object):
 
     def twoSum(self, nums, target):
